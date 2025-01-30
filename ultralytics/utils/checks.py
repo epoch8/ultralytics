39f233bbc08fe1ba1a71877a4388dedca52c202c
Alexander Kozlov <bobokvsky@epoch8.co>
2025-01-30 16:37:57 +0000

    WIP

--- ultralytics/utils/checks.py
+++ ultralytics/utils/checks.py
@@ -690,6 +690,7 @@ def check_amp(model):
 
     def amp_allclose(m, im):
         """All close FP32 vs AMP results."""
+        print(f"{im.shape=}")
         batch = [im] * 8
         imgsz = max(256, int(model.stride.max() * 4))  # max stride P5-32 and P6-64
         a = m(batch, imgsz=imgsz, device=device, verbose=False)[0].boxes.data  # FP32 inference
