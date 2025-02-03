ddc2521f33734be56f98ddb9fefa29834a1cdd4a
Alexander Kozlov <bobokvsky@epoch8.co>
2025-02-03 14:54:19 +0000

    WIP

--- ultralytics/data/augment.py
+++ ultralytics/data/augment.py
@@ -2387,7 +2387,7 @@ def v8_transforms(dataset, imgsz, hyp, stretch=False):
             RandomHSV(hgain=hyp.hsv_h, sgain=hyp.hsv_s, vgain=hyp.hsv_v),
             RandomFlip(direction="vertical", p=hyp.flipud),
             RandomFlip(direction="horizontal", p=hyp.fliplr, flip_idx=flip_idx),
-            InvertDepthChannel(p=0.5)  # Invert Depth channel with 50% probability
+            # InvertDepthChannel(p=0.5)  # Invert Depth channel with 50% probability
         ]
     )
 
